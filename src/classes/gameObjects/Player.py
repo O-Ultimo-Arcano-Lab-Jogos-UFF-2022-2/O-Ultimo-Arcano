35814270f04fc1f197184c15a372f86fc888ebf0
Rodrigo Santos Oliveira <rodrigoliveirasantos11@gmail.com>
2022-12-15 21:20:59 -0300

    Correção no merge

--- src/classes/gameObjects/Player.py
+++ src/classes/gameObjects/Player.py
@@ -132,11 +132,7 @@ class Player:
     def takeHit(self, damage):
         if (self.invincibilityCooldown == 0):
             self.invincibilityCooldown = self.absoluteInvincibilityCooldown
-<<<<<<< HEAD
             self.currentHp = self.currentHp - damage
-=======
-            self.currentHp = max(self.currentHp - damage, 0)
->>>>>>> enemy-collision
 
             if self.currentHp == 0:
                 self.window.gameOver = True
